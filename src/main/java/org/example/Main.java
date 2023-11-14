bc3d6e925f551e3845c6aae1a7345d679c017b0e
Mattia Lu <mlu481@g.educaand.es>
2023-11-14 08:51:31 +0100

    inicio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,57 @@ package org.example;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    /** Inicio codificación: RBG - 14/11/2023  */
     public static void main(String[] args) {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
+
+    public static void menu(){
+
+    }
+
+    public static int pedirNumero(){
+
+    }
+    //fin codificación: RBG - 14/11/2023  */
+    public static void trianguloArea(){
+
+    }
+    public static void trianguloPerimetro(){
+
+    }
+    public static void cuadradoArea(){
+
+    }
+    public static void cuadradoPerimetro(){
+
+    }
+    public static void rectanguloArea(){
+
+    }
+    public static void rectanguloPerimetro(){
+
+    }
+    public static void circuloArea(){
+
+    }
+    public static void circuloPerimetro(){
+
+    }
+    public static void pentagonoArea(){
+
+    }
+    public static void pentagonoPerimetro(){
+
+    }
+    public static void hexagonoArea(){
+
+    }
+    public static void hexagonoPerimetro(){
+
+    }
+
+
 }
