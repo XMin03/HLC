755b4e1b48386ac4c2bb638858259c655e1b7402
Mattia Lu <mlu481@g.educaand.es>
2023-11-16 10:02:52 +0100

    rama Mattia

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -80,8 +80,10 @@ public class Main {
         return num;
     }
     /**fin codificación: Mattia Lu - 14/11/2023  */
-    public static void trianguloArea(){
-
+    public static double trianguloArea(double b,double h){
+        double res=b*h/2;
+        System.out.println("Area: "+res);
+        return res;
     }
     public static void trianguloPerimetro(){
 
