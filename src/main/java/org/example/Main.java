2a037e3db32af76415dd58d17892d819d52ad2f7
XMin <117903450+XMin03@users.noreply.github.com>
2023-11-21 08:38:33 +0100

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    static Scanner sc=new Scanner(System.in);
+    /** Inicio codificación: Mattia Lu - 14/11/2023  */
+    public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu(){
+
+    }
+    public static double pedirNumero(String mensaje){
+    
+    }
+    /**fin codificación: Mattia Lu - 14/11/2023  */
+    public static void trianguloArea(){
+
+    }
+    public static void trianguloPerimetro(){
+
+    }
+    public static void cuadradoArea(){
+
+    }
+    public static void cuadradoPerimetro(){
+
+    }
+    public static void rectanguloArea(){
+
+    }
+    public static void rectanguloPerimetro(){
+
+    }
+    public static void circuloArea(){
+
+    }
+    public static void circuloPerimetro(){
+
+    }
+    public static void pentagonoArea(){
+
+    }
+    public static void pentagonoPerimetro(){
+
+    }
+    public static void hexagonoArea(){
+
+    }
+    public static void hexagonoPerimetro(){
+
+    }
+}
